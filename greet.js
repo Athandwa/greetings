35ce60bfe9564fc7cddaaff8edd1bbf240f8f544
ggsbv <ggsbv@protonmail.com>
2017-03-13 16:13:34 +0200

    added localStorage & a button to reset greetings

--- greet.js
+++ greet.js
@@ -1,28 +1,59 @@
 var textBox = document.querySelector(".user");
-var button = document.querySelector(".btn");
 var text = document.querySelector(".para");
 var language = document.getElementsByName("language");
-var count = document.getElementById("counter")
-//var numCounts = localStorage.getItem("on_load_counter");
-//var namesGreeted = {};
-//var button2 = document.querySelector(".resetBtn")
+var count = document.getElementById("counter");
+var button2 = document.getElementById("resetBtn");
+var namesGreeted = {};
 
 
-
+// Global Variable
 var countClicks = 0;
-button.addEventListener("click", function() {
 
-    if (language[0].checked) {
-        var textBox = document.querySelector(".user");
-        text.innerHTML = "Molo " + textBox.value + "!";
-    } else if (language[1].checked) {
-        var textBox = document.querySelector(".user");
-        text.innerHTML = "Hello " + textBox.value + "!";
-    } else if (language[2].checked) {
-        var textBox = document.querySelector(".user");
-        text.innerHTML = "Halo " + textBox.value + "!";
-    }
-    countClicks += 1;
+// retrieve variables in the browser
+var storageCounts = localStorage.getItem("countClicks");
+if (storageCounts) {
+    countClicks = Number(storageCounts);
     document.getElementById("counter").innerHTML = countClicks;
+}
+
+//created a reset button for the counter
+function reset() {
+    window.localStorage.removeItem("countClicks");
+    let countClicks = 0;
+    window.localStorage.setItem("countClicks", countClicks);
+    document.getElementById("counter").innerHTML = countClicks;
+};
+console.log(countClicks);
+
+var button = document.querySelector(".btn");
+button.addEventListener("click", function() {
+    var name = document.querySelector(".user").value;
+
+    if (language[0].checked && name.length > 0 && namesGreeted[name] === undefined) {
+        text.innerHTML = "Molo " + name + "!";
+        namesGreeted[name] = name;
+        countClicks += 1;
+    } else if (language[0].checked && namesGreeted[name] !== undefined) {
+        text.innerHTML = "Molo Kwakhona " + name + "!";
+    }
+    else if (language[1].checked && name.length > 0 && namesGreeted[name] === undefined) {
+        text.innerHTML = "Hello " + name + "!";
+        namesGreeted[name] = name;
+        countClicks += 1;
+    } else if (language[1].checked && namesGreeted[name] !== undefined) {
+        text.innerHTML = "Hello again " + name + "!";
+    }
+    else if (language[2].checked && name.length > 0 && namesGreeted[name] === undefined) {
+        text.innerHTML = "Halo " + name + "!";
+        namesGreeted[name] = name;
+        countClicks += 1;
+    } else if (language[2].checked && name.length > 0 && namesGreeted[name] !== undefined) {
+        text.innerHTML = "Halo weer " + name + "!";
+    }
+
+    document.getElementById("counter").innerHTML = countClicks;
+    // store
+    localStorage.setItem("countClicks", countClicks);
+
 });
-// alert("Welcome to the greeting webpage");
+alert("Welcome to the greeting webpage");
